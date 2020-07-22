c5b2429ac2fc939f7c79fffa0c31a057a2452478
lucadruda <ludruda@microsoft.com>
2020-07-22 16:07:58 +0200

    add appcenter versioning

---
+++ appcenter-post-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
+    cd android
+    VERSION=`./gradlew -q printVersion | tail -n 1`
+    echo $VERSION
+    git add ./gradle.properties
+    git commit -m "Android Release v$VERSION"
+    git push origin master
+    cd ..
+fi
