0f705587159fc1d84fc9a173852f8a882e30c069
lucadruda <ludruda@microsoft.com>
2020-07-22 16:07:58 +0200

    add appcenter versioning

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-cd ./ios
-pod install
+APP_CENTER_CURRENT_PLATFORM="android"
+if [ "$APP_CENTER_CURRENT_PLATFORM" == "android" ]
+then
+    cd android
+    ./gradlew increment
+    cd ..
+else
+    #iOS
+    cd ./ios
+    pod install  
+fi
