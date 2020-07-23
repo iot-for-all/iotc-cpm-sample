128da9a1af4ebd6115312a9c2c35a5cc6dc78a15
lucadruda <ludruda@microsoft.com>
2020-07-23 15:54:44 +0200

    add appcenter variables

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -3,11 +3,11 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     cd android
     VERSION=`./gradlew -q printVersion | tail -n 1`
     echo $VERSION
-    git config user.email "lucadruda@gmail.com"
-    git config user.name "lucadruda"
+    git config user.email "$GIT_EMAIL"
+    git config user.name "$GIT_USER"
     git add ./gradle.properties
-    git commit -m "Android Release v$VERSION" --author="Luca Druda <lucadruda@gmail.com>"
-    git remote set-url --push origin https://$GIT_TOKEN@github.com/lucadruda/cpm-poc.git
+    git commit -m "Android Release v$VERSION" --author="$GIT_USER <$GIT_EMAIL>"
+    git remote set-url --push origin https://$GIT_TOKEN@github.com/$GIT_USER/cpm-poc.git
     git push origin master
     cd ..
 fi
