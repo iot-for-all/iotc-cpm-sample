2b342018f351e6e1dbfd43dc2859fd1e13042fc9
Luca Druda <lucadruda@gmail.com>
2020-07-23 13:42:00 +0000

    fix post build

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -5,7 +5,7 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     echo $VERSION
     git add ./gradle.properties
     git commit -m "Android Release v$VERSION" --author="Luca Druda <lucadruda@gmail.com>"
-    git remote set-url --push origin https://$GIT_TOKEN:@github.com/lucadruda/cpm-poc.git
+    git remote set-url --push origin https://$GIT_TOKEN@github.com/lucadruda/cpm-poc.git
     git push origin master
     cd ..
 fi
